e73d8e73d00181a255916ca69f443245804ab5c3
Alesxander36 <alesxander.santos@gmail.com>
2020-06-17 06:56:00 -0300

    Aula 4 Tipos de dados Primitivos

---
+++ python_basico_(logica_de_programacao)/aula4.py
@@ -0,0 +1,29 @@
+# Tipos de dados "Primitivos"
+
+"""
+Tipos de dados
+
+str - string = texto 'Assim' "Assim"
+
+int - inteiro = 123456 0 -10 -20 -50 -60 -1500 1500
+
+float - real/ponto flutuante = 10.50 1.5 -10.10 -50.93 0.0
+
+boll - boleano/lógico = true/false 10==10
+"""
+# Exemplos:
+
+# String: nome
+print('Luiz', type('Luiz'))
+
+# Idade: Int
+print(39, type(39))
+
+# Altura: float
+print(1.88, type(1.88))
+
+# É maio de idade 18 > 39
+print(39 > 18, type(39 > 18))
+
+# Exemplo de type catch
+# print('Luiz', type('Luiz'), bool('Luiz'))
