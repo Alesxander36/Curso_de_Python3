79c6fc8abbb625581379ea79643a5bee8b14decc
Alesxander36 <alesxander.santos@gmail.com>
2020-06-17 08:52:26 -0300

    Aula 6 Variavéis

---
+++ python_basico_(logica_de_programacao)/aula6.py
@@ -0,0 +1,24 @@
+# Variavéis
+# variável é um objeto (uma posição, frequentemente localizada na memória) capaz de reter e representar um valor ou expressão.
+
+# Exemplos:
+
+nome = 'Alesxander Santos'
+idade = 39
+altura = 1.88
+e_maior = idade > 18
+data_1 = True
+data_atual = 2020
+peso = 95
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', idade, 'anos de idade e seu umc é', imc)
+
+"""
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maio:', e_maior)
+
+print(idade * altura)
+"""
