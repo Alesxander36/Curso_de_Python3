74b391a3e99d3f1dcdb28f39cd86afd922b666d1
Alesxander36 <alesxander.santos@gmail.com>
2020-06-17 05:29:17 -0300

    Aula 1 Criando cométarios

---
+++ python_basico_(logica_de_programacao)/aula1.py
@@ -0,0 +1,19 @@
+# Comandos para criar comentários em Pythons
+
+# usado para coméntarios em uma unica linha
+
+# print('Linha 1')
+print('Linha 2')
+print('Linha 3')  # esse coméntario explica o que essa linha de código executa
+print('Linha 4')
+
+"""
+Exemplos de multi linhas:
+
+print('Linha 5')
+print('Linha 6')
+"""
+
+"""
+ Usado para criar um bloco de comentários em multiplas linhas
+"""
