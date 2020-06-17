7025340c0a85f3882cbecd627470db01fac63fff
Alesxander36 <alesxander.santos@gmail.com>
2020-06-17 06:16:20 -0300

    aula 3 Função String

---
+++ python_basico_(logica_de_programacao)/aula3.py
@@ -0,0 +1,9 @@
+# Função String (é um tipo de dado primitivo em Python)
+"""
+poder ser classificado como str-string
+
+strings são textos dentros de aspas("")
+"""
+# exemplos:
+print('Essa é uma "string" (str).')
+print("Essa é uma 'string' (str).")
