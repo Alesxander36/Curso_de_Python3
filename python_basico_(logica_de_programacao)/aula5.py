961ae9279e1b77be568f5fd6beea5b15bece30aa
Alesxander36 <alesxander.santos@gmail.com>
2020-06-17 07:53:13 -0300

    Aual 5 Operadores Aritiméticos

---
+++ python_basico_(logica_de_programacao)/aula5.py
@@ -0,0 +1,10 @@
+# Operadores Aritiméticos
+
+"""
++, -, *, /, //, **, %, ()  
+"""
+
+print('Multiplicação * ', 10 * 10)
+print('Adição + ', 10 + 10)
+print('Subtração - ', 10 - 5)
+print('Dvisão / ', 10 / 2)
