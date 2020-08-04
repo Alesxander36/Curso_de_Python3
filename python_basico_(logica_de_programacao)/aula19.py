f58e752b633b3f95c9cf84f34ce51fbb1a135928
Alesxander36 <alesxander.santos@gmail.com>
2020-08-03 23:16:35 -0300

    Aula 19 - While / Else

---
+++ python_basico_(logica_de_programacao)/aula19.py
@@ -0,0 +1,22 @@
+"""
+While / Else
+
+Contadores
+acumuladores
+
+"""
+
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+
+    if contador >= 5:
+        break
+
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('Cheguei no else.')
+print('Cheguei ao fim do laço.')
